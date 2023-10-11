feb094524ee746f427b761aa06bd5d7493cb4f36
jkweber236 <jkweber236@gmail.com>
2023-10-11 14:41:04 -0600

    Completed week 5 learning

---
+++ Week 5/fetch.js
@@ -0,0 +1,95 @@
+// fetch.js
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon"; 
+let results = null;
+
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+async function getPokemonList(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        doStuffList(data);
+    }
+}
+// Activity 1
+// function doStuff(data) {
+//  const outputElement = document.querySelector("#output");
+//  results = data;
+//  const html = `<h2>${results.name}</h2>
+//                <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
+//  outputElement.innerHTML = html;
+//  console.log("first: ", results);
+//}
+
+function doStuff(data) {
+    results = data;
+    const outputElement = document.querySelector("#output");
+    const html = `<h2>${data.name}</h2><img src="${data.sprites.front_default}" alt="Image of ${data.name}">`;
+    outputElement.innerHTML = html;
+    console.log("first: ", results);
+}
+function doStuffList(data) {
+    console.log(data);
+    const pokeListElement = document.querySelector("#outputList");
+    const pokeList = data.results;
+    pokeList.forEach((currentItem) => {
+        const html = `<li>${currentItem.name}</li>`;
+        pokeListElement.innerHTML += html;
+    });
+}
+
+getPokemon(url);
+console.log("second: ", results);
+
+getPokemonList(urlList);
+
+// Alternate Solution 2 
+// const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+// const urlList = "https://pokeapi.co/api/v2/pokemon";
+// let results = null;
+
+// async function getPokemon(url, doThis) {
+//   const response = await fetch(url);
+//   //check to see if the fetch was successful
+//   if (response.ok) {
+//     // the API will send us JSON...but we have to convert the response before we can use it
+//     // .json() also returns a promise...so we await it as well.
+//     const data = await response.json();
+//     // execute the callback
+//     doThis(data);
+//   }
+// }
+
+// function doStuff(data) {
+//   results = data;
+//   const outputElement = document.querySelector("#output");
+//   const html = `<h2>${data.name}</h2><img src="${data.sprites.front_default}" alt="${data.name}">`;
+//   outputElement.innerHTML = html;
+//   console.log("first: ", results);
+// }
+
+// function doStuffList(data) {
+//   console.log(data);
+//   const pokeListElement = document.querySelector("#outputList");
+//   const pokeList = data.results;
+//   pokeList.forEach((currentItem) => {
+//     const html = `<li data-url="${item.url}">${currentItem.name}</li>`;
+//     // note the += here...
+//     pokeListElement.innerHTML += html;
+//   });
+// }
+// getPokemon(url, doStuff);
+// console.log("second: ", results);
+// // Notice that by just passing a different callback function in
+// // we can totally change what happens when the data comes back.
+// // It's like we gave the getPokemon function superpowers!
+// getPokemon(urlList, doStuffList);
